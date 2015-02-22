e90ba0481cb94608b08ee62fe9021d7a5a98b94c
Developer <>
2015-02-22 19:10:03 -0430

    add test boxes

--- PhysicsSceneKit/GameViewController.m
+++ PhysicsSceneKit/GameViewController.m
@@ -8,64 +8,59 @@
 
 #import "GameViewController.h"
 
-@implementation GameViewController
+@implementation GameViewController {
+	SCNScene *_scene;
+}
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+	[super viewDidLoad];
 
-    // create a new scene
-    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.dae"];
+	// Create a new scene
+	_scene = [SCNScene scene];
 
-    // create and add a camera to the scene
-    SCNNode *cameraNode = [SCNNode node];
+	// Create and add a camera t the scene
+   SCNNode *cameraNode = [SCNNode node];
     cameraNode.camera = [SCNCamera camera];
-    [scene.rootNode addChildNode:cameraNode];
-    
+	cameraNode.camera.yFov = 60.0f;
+    [_scene.rootNode addChildNode:cameraNode];
+
     // place the camera
-    cameraNode.position = SCNVector3Make(0, 0, 15);
-    
-    // create and add a light to the scene
-    SCNNode *lightNode = [SCNNode node];
-    lightNode.light = [SCNLight light];
-    lightNode.light.type = SCNLightTypeOmni;
-    lightNode.position = SCNVector3Make(0, 10, 10);
-    [scene.rootNode addChildNode:lightNode];
-    
-    // create and add an ambient light to the scene
-    SCNNode *ambientLightNode = [SCNNode node];
-    ambientLightNode.light = [SCNLight light];
-    ambientLightNode.light.type = SCNLightTypeAmbient;
-    ambientLightNode.light.color = [UIColor darkGrayColor];
-    [scene.rootNode addChildNode:ambientLightNode];
-    
-    // retrieve the ship node
-    SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:YES];
-    
-    // animate the 3d object
-    [ship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
-    
+	cameraNode.position = SCNVector3Make(0.0f, 0.0f, 40.0f);
+
+	// Make floor node
+	SCNNode *floorNode = [SCNNode node];
+
+	SCNFloor *floor = [SCNFloor floor];
+	floor.reflectivity = 0.25;
+	floorNode.geometry = floor;
+	floorNode.position = SCNVector3Make(0, -10, 0);
+
+	// Floor Physics
+	SCNPhysicsShape *floorShape = [SCNPhysicsShape shapeWithGeometry: floor options: nil];
+	SCNPhysicsBody *floorBody = [SCNPhysicsBody bodyWithType:SCNPhysicsBodyTypeStatic shape:floorShape];
+
+	floorNode.physicsBody = floorBody;
+
+	[_scene.rootNode addChildNode:floorNode];
+
+	[self addBoxesAtPosition:SCNVector3Make(-4.5f, 0.0f, 0.0f)];
+	[self addBoxesAtPosition:SCNVector3Make(+4.5f, 0.0f, 0.0f)];
+
     // retrieve the SCNView
     SCNView *scnView = (SCNView *)self.view;
-    
-    // set the scene to the view
-    scnView.scene = scene;
-    
-    // allows the user to manipulate the camera
-    scnView.allowsCameraControl = YES;
-        
-    // show statistics such as fps and timing information
-    scnView.showsStatistics = YES;
 
-    // configure the view
-    scnView.backgroundColor = [UIColor blackColor];
-    
-    // add a tap gesture recognizer
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
-    NSMutableArray *gestureRecognizers = [NSMutableArray array];
-    [gestureRecognizers addObject:tapGesture];
-    [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
-    scnView.gestureRecognizers = gestureRecognizers;
+	// Set the scene to the view
+	scnView.scene = _scene;
+
+	// Allows the user to manipulate the camera
+	scnView.allowsCameraControl = YES;
+
+	// Show statistics such as fps and timing information
+	scnView.showsStatistics = YES;
+
+	// Configure the view
+	scnView.backgroundColor = [UIColor blackColor];
 }
 
 - (void) handleTap:(UIGestureRecognizer*)gestureRecognize
@@ -110,7 +105,8 @@
     return YES;
 }
 
-- (BOOL)prefersStatusBarHidden {
+- (BOOL)prefersStatusBarHidden
+{
     return YES;
 }
 
@@ -127,6 +123,61 @@
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)addBoxesAtPosition:(SCNVector3)position
+{
+	// Add box nodes to the scene
+	for (int i=0; i<3; ++i)
+	{
+		for (int j=0; j<3; ++j)
+		{
+			for (int k=0; k<3; ++k)
+			{
+				SCNNode *node = [SCNNode node];
+				CGFloat rdx = 3*(i - 1) + position.x;
+				CGFloat rdy = 3*(j - 1) + position.y;
+				CGFloat rdz = 3*(k - 1) + position.z;
+				node.position = SCNVector3Make(rdx, rdy, rdz);
+				SCNBox *box = [SCNBox boxWithWidth:2.0 height:2.0 length:2.0 chamferRadius:0.0];
+				node.geometry = box;
+
+				// Create and configure a material
+				SCNMaterial *material = [SCNMaterial material];
+				material.diffuse.contents = [UIColor blueColor];
+				material.specular.contents = [UIColor blueColor];
+				material.locksAmbientWithDiffuse = true;
+
+				// Set shaderModifiers properties
+				material.shaderModifiers = @{SCNShaderModifierEntryPointSurface : @"\n"
+											 "float flakeSize = sin(u_time * 0.2);\n"
+											 "float flakeIntensity = 0.7;\n"
+											 "vec3 paintColor0 = vec3(0.9, 0.4, 0.3);\n"
+											 "vec3 paintColor1 = vec3(0.9, 0.75, 0.2);\n"
+											 "vec3 flakeColor = vec3(flakeIntensity, flakeIntensity, flakeIntensity);\n"
+											 "vec3 rnd = vec3(0.5);\n"
+											 "vec3 nrm1 = normalize(0.05 * rnd + 0.95 * _surface.normal);\n"
+											 "vec3 nrm2 = normalize(0.3 * rnd + 0.4 * _surface.normal);\n"
+											 "float fresnel1 = clamp(dot(nrm1, _surface.view), 0.0, 1.0);\n"
+											 "float fresnel2 = clamp(dot(nrm2, _surface.view), 0.0, 1.0);\n"
+											 "vec3 col = mix(paintColor0, paintColor1, fresnel1);\n"
+											 "col += pow(fresnel2, 106.0) * flakeColor;\n"
+											 "_surface.normal = nrm1;\n"
+											 "_surface.diffuse = vec4(col.r,col.b,col.g, 1.0);\n"
+											 "_surface.emission = (_surface.reflective * _surface.reflective) * 2.0;\n"
+											 "_surface.reflective = vec4(0.0);\n"};
+
+				// Set the material to the 3D object geometry
+				node.geometry.firstMaterial = material;
+
+				SCNPhysicsShape *boxShape = [SCNPhysicsShape shapeWithGeometry:box options:nil];
+				SCNPhysicsBody *boxBody = [SCNPhysicsBody bodyWithType:SCNPhysicsBodyTypeDynamic shape:boxShape];
+
+				node.physicsBody = boxBody;
+				[_scene.rootNode addChildNode:node];
+			}
+		}
+	}
 }
 
 @end
